f66496a06f4d40477f61c106fcd0777cd8e50b7c
Konstantinos <kazaziskonstantinos@gmail.com>
2023-07-19 13:09:38 +0300

    ready for post

--- cs-courses/src/components/Grid.vue
+++ cs-courses/src/components/Grid.vue
@@ -24,7 +24,7 @@ export default {
     <div class="course-container grid grid-cols-3 gap-20 max-w-1000">
       <div v-for="result in results" :key="result.id" class=" bg-gray-200 p-3 rounded-lg shadow-xl">
         <img class="w-full h-auto rounded-lg mb-10" :src="result.image" :alt="result.title" />
-        <h3 class="text-base font-medium mb-10 text-gray-700 text-center">{{ result.title }}</h3>
+        <h3 class="text-base font-medium mb-5 text-gray-700 text-center">{{ result.title }}</h3>
         <p class="text-sm mb-10 text-gray-700 font-bold">{{ result.description }}</p>
         <p class="text-sm mb-5 text-gray-700">Learners Enrolled: {{ result.enrolledLearners }}</p>
         <p class="text-sm mb-10 text-gray-700">Average Rating: {{ result.averageRating }}/5</p>
